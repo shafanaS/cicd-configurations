31b75c296bae2aafe7c3872a4058f0e5cff489ae
shafana <shafana@wso2.com>
2018-12-21 01:12:25 +0530

    Parameterize puppet modules path

--- modules/ei_integrator/manifests/init.pp
+++ modules/ei_integrator/manifests/init.pp
@@ -26,7 +26,7 @@ class ei_integrator (
     file { "$wso2_path/$product-$product_version/${template}":
       ensure  => file,
       mode    => '0644',
-      content => template("/var/lib/jenkins/workspace/EI-6.4.0/configs/modules/${module_name}/templates/carbon-home/${template}.erb")
+      content => template("${puppet_modules_path}/${module_name}/templates/carbon-home/${template}.erb")
     }
   }
 
@@ -34,7 +34,7 @@ class ei_integrator (
     file { "$wso2_path/$product-$product_version/${start_script_template}":
     ensure  => file,
     mode    => '0754',
-    content => template("/var/lib/jenkins/workspace/EI-6.4.0/configs/modules/${module_name}/templates/carbon-home/${start_script_template}.erb")
+    content => template("${puppet_modules_path}/${module_name}/templates/carbon-home/${start_script_template}.erb")
   }
 
   # Copy mysql-connector-java-5.1.41-bin.jar to installed directory
