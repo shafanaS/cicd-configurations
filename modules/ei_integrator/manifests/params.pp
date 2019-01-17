c9b41700c5b920eff50b3ad0722cdfcabfa29c53
shafana <shafana@wso2.com>
2019-01-17 16:29:35 +0530

    Change module name

--- modules/ei_integrator/manifests/params.pp
+++ modules/ei_integrator/manifests/params.pp
@@ -41,7 +41,7 @@ class ei_integrator::params {
   # $broker_elb_dns_name  = 'BROKER_ELB_DNS_NAME'
   $broker_elb_dns_name = 'localhost'
   $ei_package = 'wso2ei-6.4.0.zip'
-  $wso2_path="/var/lib/jenkins/workspace/EI-6.4.0/${module_name}r"
+  $wso2_path="/var/lib/jenkins/workspace/EI-6.4.0/${module_name}"
   $start_script_template="bin/integrator.sh"
   $mysql_connector="mysql-connector-java-5.1.41-bin.jar"
   $puppet_modules_path ="/var/lib/jenkins/workspace/EI-6.4.0/configs/modules"
